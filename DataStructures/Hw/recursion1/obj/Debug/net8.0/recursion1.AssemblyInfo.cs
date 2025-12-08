a4ef62d8ae5070dc37fe2b25fc2dbae74356a725
wizz-uu <kahlonido@gmail.com>
2025-12-08 08:51:51 +0200

    bug?

--- DataStructures/Hw/recursion1/obj/Debug/net8.0/recursion1.AssemblyInfo.cs
+++ DataStructures/Hw/recursion1/obj/Debug/net8.0/recursion1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("recursion1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44de229f7b25086ea6754fdd0c9216afb7277e23")]
 [assembly: System.Reflection.AssemblyProductAttribute("recursion1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("recursion1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
