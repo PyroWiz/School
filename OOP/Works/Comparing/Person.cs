22d7edb9b7b32121203e178c80cb62f59452b8c6
wizz-uu <kahlonido@gmail.com>
2025-12-10 16:45:26 +0200

    dadw

---
+++ OOP/Works/Comparing/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparing
+{
+    internal class Person : IComparable<Person>
+    {
+        int id; string name;
+
+        #region gettasetta
+        public Person(int id, string name)
+        {
+            SetId(id);
+            SetName(name);
+        }
+        public void SetId(int id)
+        {
+            this.id = id;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public int GetId()
+        {
+            return this.id;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+        #endregion
+
+
+        public int CompareTo(Person other)
+        {
+            if (name.CompareTo(other.name) == 0)
+            {
+                return id.CompareTo(other.id);
+            }
+            else
+            {
+                return name.CompareTo(other.name);
+            }
+
+        }
+
+    }
+}
---
+++ OOP/Works/Comparing/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person one = new Person(3, "a");
+            Person two = new Person(1, "a");
+            Person three = new Person(2, "c");
+
+            Person[] arr = new Person[] { one, two, three };
+
+           Array.Sort(arr);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i].GetName() + " ");
+                Console.WriteLine(arr[i].GetId());
+            }
+        }
+
+        public static void BubbleSort(Person[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j].CompareTo(arr[j + 1]) > 0)
+                    {
+                        Person temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
