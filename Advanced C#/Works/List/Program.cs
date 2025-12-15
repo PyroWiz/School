8f1cf41eb068d69b8d975934d71817f61502998a
wizz-uu <kahlonido@gmail.com>
2025-12-15 16:33:41 +0200

    a

---
+++ Advanced C#/Works/List/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> Dictionary = new Dictionary<string, string>();
+
+            Dictionary.Add("apple", "tapuah");
+            Dictionary.Add("bannana", "banana");
+            Dictionary.Add("Phone", "telepon");
+            Dictionary.Add("Screen", "masah");
+            Dictionary.Add("Computer", "mahshev");
+
+            Console.WriteLine("enter word in english");
+            string word = Console.ReadLine();
+
+            if (Dictionary.TryGetValue(word, out string name))
+            {
+                Console.WriteLine(name);
+            }
+            else
+            {
+                Console.WriteLine("doesnt exist in this dictionary");
+            }
+
+            Dictionary<string, List<string>> BetterDictionary = new Dictionary<string, List<string>>();
+
+            BetterDictionary.Add("apple", new List<string> { "tapah", "tapa" });
+
+            Console.WriteLine("enter word in english");
+            string aword = Console.ReadLine();
+
+            if (BetterDictionary.TryGetValue(aword, out List<string> aname))
+            {
+                foreach (string i in aname)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("doesnt exist in this dictionary");
+            }
+        }
+    }
+}
